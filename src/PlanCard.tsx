91bd4001ecc2d50b4987936f1b9acaeb9e0466db
Evgenii <evg.bazhanov@yahoo.com>
2022-11-08 23:32:17 +0100

    styling

--- src/PlanCard.tsx
+++ src/PlanCard.tsx
@@ -24,6 +24,26 @@ export default function PlanCard({
       <div className="mb-5 flex items-end text-6xl font-black">
         {price ? <div>${price}</div> : "Free"}
       </div>
+      <p className="mb-5">{description}</p>
+      <ul className="mb-10 flex flex-col gap-y">
+        {features.map((feature) => (
+          <li className="flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="mr-3 h-7 w-7"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M56.1 3.294l-6.874 15.832c2.05.889 3.957 2.073 5.663 3.515L66.037 9.468a40.12 40.12 0 00-9.936-6.174z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            {feature}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
